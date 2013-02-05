d55d5d31eb0a69ca5a25ad70d4e63162cf1c0296
DimitarChristoff <dchristoff@rulefinancial.com>
2013-02-05 14:14:28 +0000

    cli args supported

--- doctor.js
+++ doctor.js
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
-var builder = require('./lib/builder'),
-	request = require('request');
 
-request('https://raw.github.com/DimitarChristoff/Epitome/master/README.md', function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		new builder(body);
+var builder = require('./lib/builder'),
+	request = require('request'),
+	fs = require('fs');
+
+
+(function(){
+	var args = process.argv.splice(2);
+	if (!args.length){
+		console.log('Doctor needs to be passed some arguments.');
+		console.log('\tLocal: ./doctor.js README.md');
+		console.log('\tRemote: ./doctor.js https://raw.github.com/DimitarChristoff/doctor/master/README.md');
+		return;
 	}
-});
+
+	var getFile = function(uri, callback){
+		if (uri.match('http')){
+			request(uri, function(error, response, body){
+				if (!error && response.statusCode == 200){
+					callback(body);
+				}
+			});
+		}
+		else {
+			callback(fs.readFileSync(uri, 'utf-8'));
+		}
+	};
+
+	getFile(args[0], function(body){
+		new builder(body, {
+			title: 'hai'
+		});
+	});
+}());
